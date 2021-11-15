85146d1165ef8da95e139738aae9853c041a80ec
locnguyen2320 <loclocloc22032000@gmail.com>
2021-11-15 14:22:43 +0700

    them tinh hieu

--- source.cpp
+++ source.cpp
@@ -4,8 +4,8 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
-int tong(int a, int b){
-return a+b;
+int hieu(int a, int b){
+return a-b;
 }
 int main()
 {
